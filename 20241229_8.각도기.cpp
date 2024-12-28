ad0236cf8bd7afd5da88cc800f718aa4e9e1d71e
kimsewone <lmaya3@naver.com>
2024-12-29 03:59:58 +0900

    Update 20241229_8.각도기.cpp

--- 20241229_8.각도기.cpp
+++ 20241229_8.각도기.cpp
@@ -1,19 +1,21 @@
+//URL: https://school.programmers.co.kr/learn/courses/30/lessons/120829
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int solution(int num1, int num2) {
+int solution(int angle) {
     int answer = 0;
 
-    if (0 > num1)
-        num1 = 0;
-    else  if (100 < num1)
-        num1 = 100;
-    else if (0 > num2)
-        num2 = 0;
-    else  if (100 < num2)
-        num2 = 100;
+    if (0 < angle && 90 > angle)
+        answer = 1;
+    else if (90 == angle)
+        answer = 2;
+    else if (90 < angle && 180 > angle)
+        answer = 3;
+    else if (180 == angle)
+        answer = 4;
 
-    return answer = (int)(((float)num1 / (float)num2) * 1000.f);
+    return answer;
 }
