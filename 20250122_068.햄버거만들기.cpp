3ea5da55d76b6cc51d9d0b6460833cfe0456949f
kimsewone <lmaya3@naver.com>
2025-01-22 18:13:37 +0900

    Update 20250122_068.햄버거만들기.cpp

--- 20250122_068.햄버거만들기.cpp
+++ 20250122_068.햄버거만들기.cpp
@@ -2,24 +2,19 @@
 
 #include <string>
 #include <vector>
-#include <deque>
-#include <map>
-#include <stack>
 using namespace std;
 
 int solution(vector<int> ingredient) {
     int answer = 0;
-    vector<int> hamArr = { 1,2,3,1 };
     string StrHam = { "1231" };
-    string StrIngredient = "";//211231231
-    int HamIdx = 0;
-    int hamValue = 0;
+    string StrIngredient = "";
     int SucessIdx = 0;
     int nPos = 0;
-    
+    int cnt = 0 ;
+
     for (int i = 0; i < ingredient.size();++i)
         StrIngredient += to_string(ingredient[i]);//+'0';  
-        int cnt = 0 ;
+   
     while (true)
     {
         nPos = StrIngredient.find(StrHam,cnt);
