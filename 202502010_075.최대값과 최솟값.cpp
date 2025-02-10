eb641937d8dfcb02a7ddff2f192a5b40fb0ddee9
kimsewone <lmaya3@naver.com>
2025-02-10 10:37:09 +0900

    Create 202502010_075.최대값과 최솟값.cpp

---
+++ 202502010_075.최대값과 최솟값.cpp
@@ -0,0 +1,42 @@
+//URL :https://school.programmers.co.kr/learn/courses/30/lessons/12939
+
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    map<int,int> numMap;
+    int result = -1;
+    int curPos = 0;
+    int pos = 0;
+    string temp = "";
+    while (true)
+    {
+
+        temp += s[curPos] ;
+        curPos++;
+        if (s[curPos] == ' ' || curPos == s.length())
+        {
+            result = stoi(temp);
+            numMap.emplace(result, result);
+            temp = "";
+            curPos++;
+
+        }
+        if (curPos >= s.length() )
+        {
+            break;
+        }
+    }
+  
+    map<int,int>::iterator iter = numMap.begin();
+    answer += to_string(iter->first);
+    answer += ' ';
+    iter = numMap.end();
+    iter--;
+    answer += to_string(iter->first);
+
+    return answer;
+}
